489ce22416f023653fd4ca91036be4a21209f9b2
ayushkhanduri793 <125715494+ayushkhanduri793@users.noreply.github.com>
2024-03-10 10:08:10 +0530

    Update ffmpeg_helper.py

--- helpers/ffmpeg_helper.py
+++ helpers/ffmpeg_helper.py
@@ -20,7 +20,7 @@ async def MergeVideo(input_file: str, user_id: int, message: Message, format_: s
     :param `format_`: Pass File Extension.
     :return: This will return Merged Video File Path
     """
-    output_vid = f"downloads/{str(user_id)}/[@Sujan_BotZ].{format_.lower()}"
+    output_vid = f"downloads/{str(user_id)}/[MAST€R793™].{format_.lower()}"
     file_generator_command = [
         "ffmpeg",
         "-f",
@@ -95,7 +95,7 @@ async def MergeSub(filePath: str, subPath: str, user_id):
             subTrack += 1
     muxcmd.append(f"-metadata:s:s:{subTrack}")
     subTrack += 1
-    subTitle = f"Track {subTrack} - tg@Sujan_BotZ"
+    subTitle = f"Track {subTrack} - MAST€R793™"
     muxcmd.append(f"title={subTitle}")
     muxcmd.append("-c:v")
     muxcmd.append("copy")
@@ -103,7 +103,7 @@ async def MergeSub(filePath: str, subPath: str, user_id):
     muxcmd.append("copy")
     muxcmd.append("-c:s")
     muxcmd.append("srt")
-    muxcmd.append(f"./downloads/{str(user_id)}/[@Sujan_BotZ]_softmuxed_video.mkv")
+    muxcmd.append(f"./downloads/{str(user_id)}/[MAST€R793™]_softmuxed_video.mkv")
     LOGGER.info("Muxing subtitles")
     subprocess.call(muxcmd)
     orgFilePath = shutil.move(
@@ -147,7 +147,7 @@ def MergeSubNew(filePath: str, subPath: str, user_id, file_list):
         muxcmd.append("-map")
         muxcmd.append(f"{j}:s")
         muxcmd.append(f"-metadata:s:s:{subTrack}")
-        muxcmd.append(f"title=Track {subTrack+1} - tg@Sujan_BotZ")
+        muxcmd.append(f"title=Track {subTrack+1} - MAST€R793™")
         subTrack += 1
     muxcmd.append("-c:v")
     muxcmd.append("copy")
@@ -155,10 +155,10 @@ def MergeSubNew(filePath: str, subPath: str, user_id, file_list):
     muxcmd.append("copy")
     muxcmd.append("-c:s")
     muxcmd.append("srt")
-    muxcmd.append(f"./downloads/{str(user_id)}/[@Sujan_BotZ]_softmuxed_video.mkv")
+    muxcmd.append(f"./downloads/{str(user_id)}/[MAST€R793™]_softmuxed_video.mkv")
     LOGGER.info("Sub muxing")
     subprocess.call(muxcmd)
-    return f"downloads/{str(user_id)}/[@Sujan_BotZ]_softmuxed_video.mkv"
+    return f"downloads/{str(user_id)}/[MAST€R793™]_softmuxed_video.mkv"
 
 
 def MergeAudio(videoPath: str, files_list: list, user_id):
@@ -187,7 +187,7 @@ def MergeAudio(videoPath: str, files_list: list, user_id):
         muxcmd.append("-map")
         muxcmd.append(f"{j}:a")
         muxcmd.append(f"-metadata:s:a:{audioTracks}")
-        muxcmd.append(f"title=Track {audioTracks+1} - tg@Sujan_BotZ")
+        muxcmd.append(f"title=Track {audioTracks+1} - MAST€R793™")
         audioTracks += 1
     muxcmd.append(f"-disposition:s:a:{fAudio}")
     muxcmd.append("default")
@@ -199,12 +199,12 @@ def MergeAudio(videoPath: str, files_list: list, user_id):
     muxcmd.append("copy")
     muxcmd.append("-c:s")
     muxcmd.append("copy")
-    muxcmd.append(f"downloads/{str(user_id)}/[@Sujan_BotZ]_export.mkv")
+    muxcmd.append(f"downloads/{str(user_id)}/[MAST€R793™]_export.mkv")
 
     LOGGER.info(muxcmd)
     process = subprocess.call(muxcmd)
     LOGGER.info(process)
-    return f"downloads/{str(user_id)}/[@Sujan_BotZ]_export.mkv"
+    return f"downloads/{str(user_id)}/[MAST€R793™]_export.mkv"
 
 
 async def cult_small_video(video_file, output_directory, start_time, end_time, format_):
